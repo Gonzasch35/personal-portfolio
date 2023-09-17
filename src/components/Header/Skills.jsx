093eaa1950010f7c4dc878e5b848763e8e8d18d5
Gonzalo Schmidt <gonza.sch35@gmail.com>
2023-09-17 20:38:11 -0300

    skills

--- src/components/Header/Skills.jsx
+++ src/components/Header/Skills.jsx
@@ -10,6 +10,7 @@ import javascript from '../../assets/javascript.svg'
 import html from '../../assets/html.svg'
 import css from '../../assets/css.svg'
 import trello from '../../assets/trello.png'
+import python from '../../assets/python.png'
 
 const Skills = () => {
   return (
@@ -17,16 +18,62 @@ const Skills = () => {
       <h2 className={styles.skillTitle}>Skills</h2>
       <span className={styles.skillDesc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad beatae debitis fugiat porro vitae, quibusdam sit ullam a, alias natus illum? Sed esse ipsa commodi natus dignissimos, ea aut modi?</span>
       <div className={styles.skillBars}>
-          <div className={styles.image}><img className={styles.bigs} src={html} alt="" /></div>
-          <div className={styles.image}><img className={styles.bigs} src={css} alt="" /></div>
-          <div className={styles.image}><img className={styles.bigs} src={javascript} alt="" /></div>
-          <div className={styles.image}><img className={styles.bigs} src={node} alt="" /></div>
-          <div className={styles.image}><img className={styles.react} src={react} alt="" /></div>
-          <div className={styles.image}><img src={redux} alt="" /></div>
-          <div className={styles.image}><img className={styles.bigs} src={postgresql} alt="" /></div>
-          <div className={styles.image}><img className={styles.jest} src={jest} alt="" /></div>
-          <div className={styles.image}><img className={styles.git} src={git} alt="" /></div>
-          <div className={styles.image}><img className={styles.trello} src={trello} alt="" /></div>
+
+          <div className={styles.image}>
+            <img className={styles.bigs} src={html} alt="" />
+            <p>HTML5</p>
+          </div>
+
+          <div className={styles.image}>
+            <img className={styles.bigs} src={css} alt="" />
+            <p>CSS3</p>
+          </div>
+
+          <div className={styles.image}>
+            <img className={styles.bigs} src={javascript} alt="" />
+            <p>JavaScript</p>
+          </div>
+
+          <div className={styles.image}>
+            <img className={styles.bigs} src={node} alt="" />
+            <p>Node.js</p>
+          </div>
+
+          <div className={styles.image}>
+            <img className={styles.react} src={react} alt="" />
+            <p>React</p>
+          </div>
+
+          <div className={styles.image}>
+            <img className={styles.python} src={python} alt="" />
+            <p>Python</p>
+          </div>
+
+          <div className={styles.image}>
+            <img src={redux} alt="" />
+            <p>Redux</p>
+          </div>
+
+          <div className={styles.image}>
+            <img className={styles.bigs} src={postgresql} alt="" />
+            <p>PostgreSQL</p>
+          </div>
+
+          <div className={styles.image}>
+            <img className={styles.jest} src={jest} alt="" />
+            <p>Jest</p>
+          </div>
+
+          <div className={styles.image}>
+            <img className={styles.git} src={git} alt="" />
+            <p>Git</p>
+          </div>
+
+          <div className={styles.image}>
+            <img className={styles.trello} src={trello} alt="" />
+            <p>Trello</p>
+          </div>
+
       </div>
 
     </section>
